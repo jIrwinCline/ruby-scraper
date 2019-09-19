cb1f2329dd7ce498b6cc92f8ff025b1b925f7295
jirwincline <jakobinwins@gmail.com>
2019-09-19 00:16:21 -0700

    only names print

--- scraper.rb
+++ scraper.rb
@@ -9,14 +9,26 @@ def scraper
     per_page = 50
     url = "http://dpsstnet.state.or.us/IRIS_PublicInquiry/PrivateSecurity/SMSGoPersonLkp.aspx"
     url2 = "http://dpsstnet.state.or.us/IRIS_PublicInquiry/PrivateSecurity/SMSGoPersonLkp.aspx?LkpBy=ID&LkpVal=0"
-    # browser.goto(url)
-    # browser.goto(url2)
 
     # info Parsing VARIABLES\
     page = mechanize.get(url)
     page = mechanize.get(url2)
-    # parsed_page = Nokogiri::HTML(unparsed_page)
     officer = page.css('font').css('a')
+    right_link = false
+    count = 0
+    officer.each do |i|
+        if count > 3
+            if right_link
+                puts i.text
+                right_link = false
+            else
+                right_link = true
+            end
+        else 
+            count += 1
+        end
+    end
+
     byebug
 end
 
